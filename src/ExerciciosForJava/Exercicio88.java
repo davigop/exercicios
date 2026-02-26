cab63dd2665827ff4fddafae059ab895ce04ba18
davigop <davigsxestudos@gmail.com>
2026-02-26 18:39:03 -0300

    exercicoFor

---
+++ src/ExerciciosForJava/Exercicio88.java
@@ -0,0 +1,13 @@
+/**
+ * OBJETIVO:Some os números de 1 a 100 e imprima o valor.
+ * ENTRADA:
+ * SAÍDA:
+ */
+void main() {
+    int total = 0;
+    for (int i = 1; i <= 100; i++) {
+        total = total + i;
+
+    }
+    System.out.println(total);
+}
