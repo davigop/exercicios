aedca3e2e0e26bf1379c18f974ebdfabedd23c49
davigop <davigsxestudos@gmail.com>
2026-02-27 20:46:38 -0300

    exercicioFor

---
+++ src/Exercicio94A.java
@@ -0,0 +1,15 @@
+/**
+ * OBJETIVO:Dado o conjunto de instruções a seguir, faça um algoritmo com quatro variações,
+ * colocando o comando de repetição adequadamente, de forma a:
+ * a) Executar o conjunto 10 vezes;
+ * b) Não executar nenhuma vez;
+ * c) Executar o conjunto 100 vezes utilizando duas estruturas de repetição;
+ * d) Executar N vezes, onde N é uma variável informada pelo usuário.
+ * ENTRADA:
+ * SAÍDA:
+ */
+void main() {
+    for (int i = 1; i <= 10; i++) {
+        System.out.println(i);
+    }
+}
---
+++ src/Exercicio94D.java
@@ -0,0 +1,7 @@
+void main() {
+    System.out.println("Quantas vezes será executado?");
+    int valorN = new Scanner(System.in).nextInt();
+    for (int i = 1; i <= valorN; i++) {
+        System.out.println(i);
+    }
+}
